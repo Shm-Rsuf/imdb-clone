c37af4a252e7457ac05db7651c3020bbae14aaa9
shm_usuf <shmusuf@gmail.com>
2023-08-04 19:28:16 +0600

    header added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,27 @@
+import Link from "next/link";
+import MenuItem from "./MenuItem";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+
 const Header = () => {
-  return <section>Header</section>;
+  return (
+    <header className="container mx-auto px-5 lg:px-0 flex justify-between items-center py-4">
+      <div className="flex justify-center items-center gap-5">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+      <div className="">
+        <Link href="/">
+          <h2 className="text-2xl">
+            <span className="font-semibold bg-amber-500 px-2 py-1 rounded-lg mr-1">
+              IMDb
+            </span>
+            <span className="text-xl hidden sm:inline">Clone</span>
+          </h2>
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
