d46cef01ad0f41da50bad633b8d3fa4ccc7351a2
shm_usuf <shmusuf@gmail.com>
2023-08-04 20:53:11 +0600

    dark mode added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* HEADER */}
-        <Header />
+        <Providers>
+          {/* HEADER */}
+          <Header />
 
-        {children}
+          {children}
+        </Providers>
       </body>
     </html>
   );
---
+++ components/DarkModeSwitch.jsx
@@ -0,0 +1,31 @@
+"use client";
+import { MdLightMode } from "react-icons/md";
+import { FaMoon } from "react-icons/fa";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+const DarkModeSwitch = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  useEffect(() => setMounted(true), [setMounted]);
+
+  return (
+    <>
+      {mounted && currentTheme === "dark" ? (
+        <MdLightMode
+          className="text-xl cursor-pointer hover:text-amber-500 duration-300"
+          onClick={() => setTheme("light")}
+        />
+      ) : (
+        <FaMoon
+          className="text-xl cursor-pointer hover:text-amber-500 duration-300"
+          onClick={() => setTheme("dark")}
+        />
+      )}
+    </>
+  );
+};
+
+export default DarkModeSwitch;
