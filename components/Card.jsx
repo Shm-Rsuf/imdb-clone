b7ab1428bea447e7b66734ca4c9c5b398b13429b
shm_usuf <shmusuf@gmail.com>
2023-08-05 21:03:25 +0600

    add card component

---
+++ components/Card.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import { BsHandThumbsUp } from "react-icons/bs";
+
+const Card = ({ result }) => {
+  console.log(result);
+  return (
+    <div className="p-2 my-5 cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-300 group">
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          alt="image not found"
+          width={350}
+          height={500}
+          placeholder="blur"
+          blurDataURL="/spin.svg"
+          className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300 w-full object-cover"
+          style={{ maxWidth: "100%", height: "auto" }}
+        />
+      </Link>
+      <div className=" space-y-2">
+        <p className="">{result.overview.substring(0, 50)}...</p>
+        <h3 className="font-semibold truncate text-lg">
+          {result.title || result.name}
+        </h3>
+        <p className="flex items-center gap-5">
+          {result.release_date || result.first_air_date}
+          <span className="flex items-center gap-[2px]">
+            <BsHandThumbsUp /> {result.vote_count}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
