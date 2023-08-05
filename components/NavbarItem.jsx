ba50810d3f220a7f6fc258918c58417eb1a5f19c
shm_usuf <shmusuf@gmail.com>
2023-08-05 11:02:03 +0600

    navvar component create

--- components/NavbarItem.jsx
+++ components/NavbarItem.jsx
@@ -1,11 +1,21 @@
+"use client";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 const NavbarItem = ({ title, param }) => {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+  console.log("genre =", genre);
+
   return (
     <div>
       <Link
         href={`/?genre=${param}`}
-        className={`font-semibold hover:text-amber-600 cursor-pointer duration-300`}
+        className={`font-semibold hover:text-amber-600 cursor-pointer duration-300 ${
+          genre &&
+          genre === param &&
+          "underline underline-offset-8 decoration-[3px] decoration-amber-500 rounded-lg"
+        }`}
       >
         <h2>{title}</h2>
       </Link>
