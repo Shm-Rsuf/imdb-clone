9afc62d371bf16689bb96609238b7f12abd0d262
shm_usuf <shmusuf@gmail.com>
2023-08-06 13:51:51 +0600

    searchTerm added

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const SearchBox = () => {
-  const [input, setInput] = useState("");
+  const [inputText, setInputText] = useState("");
+  const router = useRouter();
 
   /* handleInputSubmit */
   const handleInputSubmit = (e) => {
     e.preventDefault();
+    router.push(`/search/${inputText}`);
+    console.log("clicked");
   };
 
   return (
@@ -17,14 +21,15 @@ const SearchBox = () => {
         className="wrapper flex justify-between items-center py-4 gap-2"
       >
         <input
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
+          onChange={(e) => setInputText(e.target.value)}
+          value={inputText}
           required
           type="text"
           placeholder="Search keywords.."
           className="w-full px-2 h-14 rounded-md bg-transparent outline-none placeholder:gray-500 flex-1 border dark:border-gray-600"
         />
         <button
+          disabled={!inputText}
           type="submit"
           className="text-amber-500 disabled:text-amber-400"
         >
