6de386d7ad9ce79360c79d7f234f0cdd95a8d9c7
shm_usuf <shmusuf@gmail.com>
2023-08-06 19:53:33 +0600

    data fetch successfully

--- app/search/[searchTerm]/page.jsx
+++ app/search/[searchTerm]/page.jsx
@@ -1,5 +1,17 @@
-const SearchPage = async () => {
-  return <div>SearchPage</div>;
+const SearchPage = async ({ params }) => {
+  // const id = params.searchTerm;
+  // console.log("searchId: ", id);
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${params.searchTerm}&language=en-US`
+  );
+  if (!response.ok) {
+    throw new Error("Error featching data");
+  }
+
+  const data = await response.json();
+  const results = data.results;
+
+  return <div>helo</div>;
 };
 
 export default SearchPage;
